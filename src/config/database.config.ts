ed3e4f6e86e05f194f450e0afb747fb1c00448d4
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2025-12-15 14:57:53 +0100

    update : synchronize turned to true

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,7 +11,7 @@ export default registerAs(
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    synchronize: process.env.NODE_ENV !== 'production', // true en dev, false en prod
+    synchronize: true, // TypeORM crée/modifie les tables automatiquement
     logging: process.env.NODE_ENV === 'development',
     ssl:
       process.env.DATABASE_SSL === 'true'
